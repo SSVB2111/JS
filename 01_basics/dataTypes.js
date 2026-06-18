efa717aba7964d0be21a0603bfddc36dc60df652
Shlok Bhoyar <shlokbhoyar2105@gmail.com>
2026-06-18 18:11:52 +0530

    dataTypes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -1,1 +1,12 @@
 "use strict"; // treat all js code as of new version
+
+let a = 10
+let b = "shlok"
+let c = null // null -> object
+
+console.log(typeof a);
+console.log(typeof 90);
+console.log(typeof b);
+console.log(typeof "bhoyar");
+console.log(typeof undefined);
+console.log(typeof null);
