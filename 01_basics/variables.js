7726e7e635d7ab512cad77f3a1f25008636c3c04
Shlok Bhoyar <shlokbhoyar2105@gmail.com>
2026-06-18 17:59:21 +0530

    dataTypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountNo = 7839202083;
+let accountBal = "782202020";
+var accountJoint = "- Joint Account Holder's Name -";
+accountName = "Rajesh Pilot"
+let accountUpdate;
+
+console.log(accountNo);
+
+console.table([accountBal, accountJoint, accountName, accountUpdate]);
